434e07fb1e2eb839d0cf5eda3447c190d5a02204
MightyPentium4 <intel.darren@gmail.com>
2024-09-25 18:56:22 -0400

    PostgreSQL (Travel Tracker part 3)

--- 33.3+Travel+Tracker/index.js
+++ 33.3+Travel+Tracker/index.js
@@ -17,17 +17,56 @@ const db = new pg.Client({
 });
 db.connect();
 
-app.get("/", async (req, res) => {
-  //Write your code here.
-  const globe = await db.query("SELECT flag FROM flags WHERE name NOT LIKE 'I%' AND name LIKE '%a';");
+async function checkVisisted(){
+  const globe = await db.query("SELECT country_code FROM visited_countries;");
   let countries = [];
-  globe.rows.forEach((flags) => {
-    countries.push(flags.flag);
+  globe.rows.forEach((country) => {
+    countries.push(country.country_code);
   });
   console.log(globe.rows);
+  return countries;
+}
+
+// Get homepage
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
   res.render("index.ejs", { countries: countries, total: countries.length });
-  db.end();
 });
+
+// Insert country
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';", // ignores capitalization and can check parts of the word
+      [input.toLowerCase()]
+    );
+
+    const countryCode = result.rows[0].country_code; // Selects first result that returns from the db
+    try {
+      await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [countryCode]);
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+      const countries = await checkVisisted();
+      errorMessage("Country has already been added, try again.", res);
+    }
+  } catch (err) {
+    console.log(err);
+    const countries = await checkVisisted();
+    errorMessage("Country name does not exist, try again.", res);
+  }
+});
+
+async function errorMessage(message, res){
+  const reCheck = await checkVisisted();
+    res.render("index.ejs", {
+      countries: reCheck,
+      total: reCheck.length,
+      error: message,
+    });
+}
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
