f4c6c8c97068146dac75eb0aed272965946b663c
MightyPentium4 <intel.darren@gmail.com>
2025-03-04 15:40:24 -0500

    React (JSX Introduction)

---
+++ 35.0+React/279-intro-to-jsx/src/index.jsx
@@ -0,0 +1,24 @@
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <div>
+        <h1>Hello Woow</h1>
+        <p>This is what must be done</p>
+        <ol>
+            <li>Hostile takeover of Cognizant and TheHartford</li>
+            <li>Fire everyone with last names longer than 2 characters</li>
+            <li>Profit???</li>
+        </ol>
+    </div>, 
+document.getElementById("root"));
+
+var h1 = document.createElement("h1");
+h1.innerHTML = "Hello Wew🦁";
+document.getElementById("root").appendChild(h1);
