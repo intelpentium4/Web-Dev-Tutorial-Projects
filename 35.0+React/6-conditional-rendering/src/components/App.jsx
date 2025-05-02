806f691f0d56c900c517043de529222fa8abdb7b
MightyPentium4 <intel.darren@gmail.com>
2025-05-02 11:34:27 -0400

    React (Conditional Rendering with

--- 35.0+React/6-conditional-rendering/src/components/App.jsx
+++ 35.0+React/6-conditional-rendering/src/components/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Login from "./Login";
 
 var isLoggedIn = false;
+var isRegistered = false;
 
 function App() {
   return (
     <div className="container">
-      {isLoggedIn ? <h1>Hello User!</h1> : <Login /> /* ternary or conditional operator */}
+      {isLoggedIn ? <h1>Hello User!</h1> : <Login Registered={isRegistered}/> /* ternary or conditional operator */}
     </div>
   );
 }
--- 35.0+React/6-conditional-rendering/src/components/Login.jsx
+++ 35.0+React/6-conditional-rendering/src/components/Login.jsx
@@ -3,14 +3,15 @@ import Input from "./Input";
 
 const currentTime = new Date(2021, 1, 20, 18).getHours();
 
-function Login(){
+function Login(props){
     return (
     <div>
         {currentTime > 12 ? <h1>Why are you still working?</h1> : null}
         <form className="form">
         <Input type="text" placeholder="Username" />
         <Input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
+        {props.Registered === false && (<Input type="password" placeholder="Confirm Password" />) /* This "and" logic acts like a condition */}
+        <button type="submit">{props.Registered ? "Login" : "Register"}</button>
         </form>
     </div>
     );
